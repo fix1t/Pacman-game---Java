7f560638504f4568f66e75ffb6281b8413eaf11c
Fix1t <gbiel1117@gmail.com>
2023-03-07 12:04:46 +0100

    TEST1, TEST2, TEST3 pass

--- game/MazeConfigure.java
+++ game/MazeConfigure.java
@@ -1,73 +1,114 @@
 package ija.ija2022.homework1.game;
 
+import ija.ija2022.homework1.common.Field;
 import ija.ija2022.homework1.common.Maze;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class MazeConfigure {
+    private static final int BORDER = 2;
     boolean pacmanPlaced;
     boolean started;
     int rows;
     int cols;
     boolean errorFlag;
-    List<String> configuration = new ArrayList<String>();
+    int currentRow;
+    Field[][] fields;
+
     //constructor
-    public MazeConfigure(){
+    public MazeConfigure() {
         this.pacmanPlaced = false;
         this.rows = 0;
         this.cols = 0;
+        this.currentRow = 0;
         this.started = false;
         this.errorFlag = false;
     }
 
     public void startReading(int rows, int cols) {
-        int border = 0;
-        this.rows = rows+border;
-        this.cols = cols+border;
+        this.rows = rows + BORDER;
+        this.cols = cols + BORDER;
         this.started = true;
+        this.fields = new Field[rows + BORDER][cols + BORDER];
     }
+
     public boolean processLine(String line) {
-        if (!this.started || this.cols != line.length()){
-            this.errorFlag = true;
-            return  false;
-        }
-
-        if (this.configuration.size() >= this.rows){
+        if (!this.started || this.cols - BORDER != line.length()) {
             this.errorFlag = true;
             return false;
         }
 
-        //check allowed chars & only one occurrence of S
-        String allowedCharsRegex = "^[X.]*S?[X.]*$";
-        Pattern p = Pattern.compile(allowedCharsRegex);
-        Matcher m = p.matcher(line);
-        if (!m.matches()) {
+        if (this.currentRow >= this.rows - BORDER) {
             this.errorFlag = true;
             return false;
         }
-        //check if pacman was placed
-        String containsStart = "^[X.]*S?[X.]*$";
-        p = Pattern.compile(containsStart);
-        m = p.matcher(line);
-        if (m.matches()){
-            this.errorFlag = true;
-            this.pacmanPlaced=true;
+        this.currentRow++;
+        for (int i = 0; i < line.length(); i++) {
+            switch (line.charAt(i)) {
+                case '.':
+                    fields[this.currentRow][i + 1] = new PathField(this.currentRow, i + 1);
+                    break;
+                case 'X':
+                    fields[this.currentRow][i + 1] = new WallField(this.currentRow, i + 1);
+                    break;
+                case 'S':
+                    if (this.pacmanPlaced) {
+                        this.errorFlag = true;
+                        return false;
+                    } else {
+                        this.pacmanPlaced = true;
+                        fields[this.currentRow][i + 1] = new PathField(this.currentRow, i + 1);
+                        fields[this.currentRow][i + 1].put(new PacmanObject());
+                    }
+                    break;
+
+                default:
+                    this.errorFlag = true;
+                    return false;
+            }
         }
 
-        this.configuration.add(line);
         return true;
     }
 
-
-
     public boolean stopReading() {
-        return this.configuration.size() == this.rows && this.started && !this.errorFlag;
+        return currentRow + 1 == this.rows && this.started && !this.errorFlag;
     }
 
     public Maze createMaze() {
-        Maze
+        if (this.errorFlag)
+            return null;
+        else {
+
+            //create border wall
+            for (int i = 0; i < this.rows; i++) {
+                for (int j = 0; j < this.cols; j++) {
+                    if (i == 0 || i == this.rows - 1) {
+                        this.fields[i][j] = new WallField(i, j);
+                        continue;
+                    }
+                    if (j == 0 || j == this.cols - 1) {
+                        this.fields[i][j] = new WallField(i, j);
+                    }
+                }
+            }
+            return new Maze() {
+
+                @Override
+                public int numCols() {
+                    return cols;
+                }
+
+                @Override
+                public int numRows() {
+                    return rows;
+                }
+
+                @Override
+                public Field getField(int row, int col) {
+                    if (row > numRows() || col > numCols())
+                        return null;
+                    return fields[row][col];
+                }
+            };
+        }
     }
 }
--- game/WallField.java
+++ game/WallField.java
@@ -1,9 +1,82 @@
 package ija.ija2022.homework1.game;
 
-public class WallField {
-    public WallField(int i, int c) {
+import ija.ija2022.homework1.common.Field;
+import ija.ija2022.homework1.common.Maze;
+import ija.ija2022.homework1.common.MazeObject;
+
+public class WallField implements Field {
+    int x;
+    int y;
+
+    public WallField(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public static class PacmanObject {
+    @Override
+    public int getX() {
+        return this.x;
+    }
+
+    @Override
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public void setMaze(Maze maze) {
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.getClass() == obj.getClass()) {
+            Field f1 = (Field) obj;
+            if (f1.getX() == this.getX())
+                return f1.getY() == this.getY();
+            return false;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Field nextField(Direction dirs) {
+        return null;
+    }
+
+    @Override
+    public boolean put(MazeObject object) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public boolean remove(MazeObject object) {
+        return false;
+    }
+
+    @Override
+    public MazeObject get() {
+        return null;
+    }
+
+    @Override
+    public boolean canMove() {
+        return false;
     }
 }
