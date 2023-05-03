77095b70ad5e13d6d7a90ab44dad81e54890908c
Mikynek <mikysek.jakub@gmail.com>
2023-05-03 14:52:24 +0200

    docs: updated JavaDoc comments example

--- ija/ija2022/homework2/tool/Sound.java
+++ ija/ija2022/homework2/tool/Sound.java
@@ -5,12 +5,30 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.net.URL;
 
+/**
+ * Class for processing sound files and manipulation with them.
+ * @author Jakub Mikyšek
+ */
 public class Sound {
   Clip clip;
+
+  /**
+   * Stores all the paths to sound files in array.
+   */
   URL[] soundURL = new URL[10];
+
+  /**
+   * Fill {@code soundURL} array with sound file paths.
+   */
   public Sound() {
     soundURL[0] = getClass().getResource("./lib/PacmanSoundtrack.wav");
   }
+
+  /**
+   * Access sound files and opens them.
+   *
+   * @param i index to access sound files in soundURL array
+   */
   public void setFile(int i) {
     try {
       AudioInputStream music = AudioSystem.getAudioInputStream(soundURL[i]);
@@ -21,14 +39,23 @@ public class Sound {
     }
   }
 
+  /**
+   * Start audio playback.
+   */
   public void play() {
     clip.start();
   }
 
+  /**
+   * Continuously audio loop playback.
+   */
   public void loop() {
     clip.loop(Clip.LOOP_CONTINUOUSLY);
   }
 
+  /**
+   * Stop audio playback.
+   */
   public void stop() {
     clip.stop();
   }
