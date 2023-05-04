59cd29b39b8d634a31d6fc1cd776e542c5546274
Fix1t <gbiel1117@gmail.com>
2023-05-04 17:07:06 +0200

    chore: comments for maze, ghostObject, Game

--- ija/ija2022/homework2/game/Game.java
+++ ija/ija2022/homework2/game/Game.java
@@ -17,6 +17,9 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * The `Game` class represents a game of Pacman.
+ */
 public class Game {
   CommonMaze maze;
   Sound sound = new Sound();
@@ -27,27 +30,47 @@ public class Game {
   List<CommonMazeObject> allMazeObjects;
   JFrame frame;
 
+  /**
+   * Creates a new game with default settings.
+   */
   public Game() {
     this.tickLength = 500;
     this.pauseGhosts = false;
   }
 
+  /**
+   * Creates a new game with the specified game speed and ghost pause setting.
+   *
+   * @param gameSpeed    the delay between game ticks in milliseconds.
+   * @param pauseGhosts  whether the ghosts should be paused during the game.
+   */
   public Game(int gameSpeed, boolean pauseGhosts) {
     this.tickLength = gameSpeed;
     this.pauseGhosts = pauseGhosts;
   }
 
+  /**
+   * Runs the game with the maze specified in the command-line argument.
+   *
+   * @param args  command-line arguments (not used).
+   */
   public static void main(String[] args) {
     Game game = new Game();
     //TODO: load maze given in argument
     Path pathToMaze = Path.of("ija/ija2022/homework2/tool/tests/maps/valid/valid0");
-    if( game.play(pathToMaze))
+    if (game.play(pathToMaze))
       System.out.println("Game ended successfully");
     else
       System.out.println("Game ended with error");
   }
 
-  public CommonMaze createMazeFromFile (Path pathToMaze) {
+  /**
+   * Creates a `CommonMaze` object from the specified file.
+   *
+   * @param pathToMaze    the path to the maze file.
+   * @return              the `CommonMaze` object representing the maze.
+   */
+  public CommonMaze createMazeFromFile(Path pathToMaze) {
     try (InputStream inputStream = Files.newInputStream(pathToMaze)) {
       this.maze = this.createMaze(inputStream);
     } catch (IOException e) {
@@ -63,11 +86,20 @@ public class Game {
     return this.maze;
   }
 
+  /**
+   * Creates a `CommonMaze` object from the specified input stream.
+   *
+   * @param inputStream   the input stream to read the maze from.
+   * @return              the `CommonMaze` object representing the maze.
+   */
   public CommonMaze createMaze(InputStream inputStream) {
     MazeConfigure mazeConfigure = new MazeConfigure();
     return mazeConfigure.loadMaze(inputStream);
   }
 
+  /**
+   * Creates the graphical user interface for the game.
+   */
   public void createGameGUI() {
     this.frame = new JFrame("Pacman Demo");
     this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -80,6 +112,12 @@ public class Game {
     presenter.open();
   }
 
+  /**
+   * Plays the game with the specified maze file.
+   *
+   * @param pathToMaze    the path to the maze file.
+   * @return              `true` if the game ended successfully, `false` otherwise.
+   */
   public boolean play(Path pathToMaze) {
     //open stream to file & load maze
     this.maze = this.createMazeFromFile(pathToMaze);
@@ -92,6 +130,9 @@ public class Game {
     return true;
   }
 
+  /**
+   * Runs the main game loop until Pacman wins or dies.
+   */
   public void gameLoop() {
     this.setAllMazeObjects();
     PacmanObject pacman = this.maze.getPacman();
@@ -101,15 +142,18 @@ public class Game {
     } while (!pacman.isDead() && !pacman.isVictorious());
   }
 
+  /**
+   * Sets the list of all `CommonMazeObject` instances in the maze.
+   */
   public void setAllMazeObjects() {
     this.allMazeObjects = this.maze.getGhosts();
     this.allMazeObjects.add(this.maze.getPacman());
   }
 
   /**
-   * Starts playing music.
+   * Starts playing music with the specified song index.
    *
-   * @param songIndex index of song in {@code songURL[]}.
+   * @param songIndex     the index of the song to play.
    */
   public void playMusic(int songIndex) {
     sound.setFile(songIndex);
@@ -124,6 +168,11 @@ public class Game {
     sound.stop();
   }
 
+  /**
+   * Sleeps the current thread for the specified number of milliseconds.
+   *
+   * @param ms    the number of milliseconds to sleep.
+   */
   public static void sleep(int ms) {
     try {
       Thread.sleep(ms);
@@ -132,6 +181,9 @@ public class Game {
     }
   }
 
+  /**
+   * Moves all `CommonMazeObject` instances in the maze.
+   */
   private void moveAllMazeObjects() {
     for (CommonMazeObject mazeObject : this.allMazeObjects) {
       if (mazeObject.getType() == ObjectType.GHOST && this.pauseGhosts)
--- ija/ija2022/homework2/game/Maze.java
+++ ija/ija2022/homework2/game/Maze.java
@@ -18,6 +18,12 @@ public class Maze implements CommonMaze {
   TargetObject target;
   private Map<PathField,CommonMazeObject> initialObjectsLayout;
 
+  /**
+   * Constructs a new Maze with the specified number of columns and rows.
+   *
+   * @param col  the number of columns in the Maze.
+   * @param row  the number of rows in the Maze.
+   */
   public Maze(int col, int row) {
       this.cols = col;
       this.rows = row;
@@ -29,20 +35,43 @@ public class Maze implements CommonMaze {
       this.initialObjectsLayout = null;
     }
 
+  /**
+   * Sets the CommonField array representing the Maze.
+   *
+   * @param fields  the CommonField array representing the Maze.
+   */
     public void setFields(CommonField[][] fields) {
         this.fields = fields;
     }
 
+  /**
+   * Returns the number of columns in the Maze.
+   *
+   * @return the number of columns in the Maze.
+   */
     @Override
     public int numCols() {
         return this.cols;
     }
 
+  /**
+   * Returns the number of rows in the Maze.
+   *
+   * @return the number of rows in the Maze.
+   */
     @Override
     public int numRows() {
           return this.rows;
       }
 
+
+  /**
+   * Returns the CommonField object at the specified coordinates in the Maze.
+   *
+   * @param row  the row coordinate of the CommonField.
+   * @param col  the column coordinate of the CommonField.
+   * @return the CommonField object at the specified coordinates in the Maze, or null if the coordinates are out of bounds.
+   */
     @Override
     public CommonField getField(int row, int col) {
         if (row < 0 || row > this.numRows())
@@ -52,27 +81,50 @@ public class Maze implements CommonMaze {
         return fields[row][col];
     }
 
+  /**
+   * Returns a copy of the list of ghosts in the Maze.
+   *
+   * @return a copy of the list of ghosts in the Maze.
+   */
     @Override
-    // return copy of list of ghosts
     public List<CommonMazeObject> getGhosts() {
       return new ArrayList<>(this.listOfGhosts);
     }
+
+  /**
+   * Returns a copy of the list of keys in the Maze.
+   *
+   * @return a copy of the list of keys in the Maze.
+   */
     @Override
-    // return copy of list of keys
     public  List<CommonMazeObject> getKeys() {
       return new ArrayList<>(this.listOfKeys);
     }
 
+  /**
+   * Returns the target object in the Maze.
+   *
+   * @return the target object in the Maze, or null if there is no target object.
+   */
   @Override
   public CommonMazeObject getTarget() {
     return this.target;
   }
 
+  /**
+   * Returns the Pacman object in the Maze.
+   *
+   * @return the Pacman object in the Maze, or null if there is no Pacman object.
+   */
   @Override
   public PacmanObject getPacman() {
     return this.pacman;
   }
 
+  /**
+   * Restores the initial state of the Maze by clearing all fields and objects and then restoring the initial
+   * objects' layout.
+   */
   @Override
   public void restore() {
     // clear all fields
@@ -106,6 +158,9 @@ public class Maze implements CommonMaze {
     }
   }
 
+  /**
+   * Clears all fields in the Maze.
+   */
   private void clearAllFields() {
     for (int i = 0; i < this.numRows(); i++) {
       for (int j = 0; j < this.numCols(); j++) {
@@ -114,22 +169,47 @@ public class Maze implements CommonMaze {
     }
   }
 
+  /**
+   * Sets the list of ghosts in the Maze to the specified list.
+   *
+   * @param listOfGhosts  the list of ghosts to set in the Maze.
+   */
   public void setGhostList(List<CommonMazeObject> listOfGhosts) {
       this.listOfGhosts = listOfGhosts;
   }
 
+  /**
+   * Sets the list of keys in the Maze to the specified list.
+   *
+   * @param listOfKeys  the list of keys to set in the Maze.
+   */
   public void setKeysList(List<CommonMazeObject> listOfKeys) {
       this.listOfKeys = listOfKeys;
   }
 
+  /**
+   * Sets the Pacman object in the Maze to the specified object.
+   *
+   * @param pacman  the Pacman object to set in the Maze.
+   */
   public void setPacman(PacmanObject pacman) {
       this.pacman = pacman;
   }
 
+  /**
+   * Sets the target object in the Maze to the specified object.
+   *
+   * @param target  the target object to set in the Maze.
+   */
   public void setTarget(TargetObject target) {
       this.target = target;
   }
 
+  /**
+   * Sets the initial objects layout of the Maze to the specified map.
+   *
+   * @param initialObjectsLayout  the initial objects layout of the Maze.
+   */
   public void setInitialObjectsLayout(Map<PathField, CommonMazeObject> initialObjectsLayout) {
     this.initialObjectsLayout = initialObjectsLayout;
   }
