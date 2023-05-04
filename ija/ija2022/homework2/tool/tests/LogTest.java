838b35f33729f3008ac033e7396f62ff1c1aa81b
Fix1t <gbiel1117@gmail.com>
2023-05-04 21:43:37 +0200

    feat: #39 replay capture

--- ija/ija2022/homework2/tool/tests/LogTest.java
+++ ija/ija2022/homework2/tool/tests/LogTest.java
@@ -3,10 +3,16 @@ package ija.ija2022.homework2.tool.tests;
 import ija.ija2022.homework2.game.Game;
 import ija.ija2022.homework2.game.PacmanObject;
 import ija.ija2022.homework2.tool.common.CommonMaze;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import static ija.ija2022.homework2.tool.common.CommonField.Direction.UP;
 
@@ -24,7 +30,7 @@ public class LogTest {
 //GXS
   @Before
   public void setUp() {
-    this.game = new Game(300, true);
+    this.game = new Game(100, false);
     this.maze = this.game.createMazeFromFile(Path.of("ija/ija2022/homework2/tool/tests/maps/valid/twoLines"));
     if (maze == null) {
       System.out.println("Error while loading maze");
@@ -35,6 +41,23 @@ public class LogTest {
   public void testLog() {
     PacmanObject pacman = maze.getPacman();
     pacman.setDirection(UP);
+    game.gameLoop(4);
+    this.game.finishRecording();
+    boolean isEqual = areFilesEqual("game.log", "ija/ija2022/homework2/tool/tests/expected/expectedLog");
+    Assert.assertTrue(isEqual);
+  }
 
+  // compare log file with expected log file
+  public static boolean areFilesEqual(String file1, String file2)  {
+    try {
+      Path file1Path = Paths.get(file1);
+      Path file2Path = Paths.get(file2);
+      byte[] f1 = Files.readAllBytes(file1Path);
+      byte[] f2 = Files.readAllBytes(file2Path);
+      return Arrays.equals(f1, f2);
+    } catch (IOException e) {
+      System.out.println("Error while reading files");
+      return false;
+    }
   }
 }
