0d2e34764ae436c714b54298c147951418df395e
Mikynek <mikysek.jakub@gmail.com>
2023-05-06 16:51:40 +0200

    fix: GUI -> Menu -> soundIcon position

--- ija/ija2022/homework2/tool/MazeMenu.java
+++ ija/ija2022/homework2/tool/MazeMenu.java
@@ -8,8 +8,8 @@ import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
 public class MazeMenu {
   private JFrame frame;
   private Sound sound;
-  JButton soundLabel;
+  JButton soundButton;
   Font customFont;
   Font headerFont;
 
@@ -42,7 +42,7 @@ public class MazeMenu {
     try {
       SwingUtilities.invokeAndWait(this::initializeInterface);
     } catch (InvocationTargetException | InterruptedException var2) {
-      Logger.getLogger(Menu.class.getName()).log(Level.SEVERE, (String)null, var2);
+      Logger.getLogger(Menu.class.getName()).log(Level.SEVERE, (String) null, var2);
     }
 
   }
@@ -59,7 +59,7 @@ public class MazeMenu {
     content.setBorder(BorderFactory.createEmptyBorder(200, 0, 0, 0));
 
     JLabel heading = new JLabel("PACMAN");
-    heading.setForeground(new Color(251,227,67)); // change color to red when mouse enters
+    heading.setForeground(new Color(251, 227, 67)); // change color to red when mouse enters
     heading.setAlignmentX(Component.CENTER_ALIGNMENT); // center horizontally
     heading.setFont(headerFont);
     content.add(heading);
@@ -74,25 +74,31 @@ public class MazeMenu {
     final boolean[] soundOn = {sound.isPlaying()};
     ImageIcon soundOnIcon = new ImageIcon(getClass().getResource("../tool/lib/iconSound.png"));
     ImageIcon soundOffIcon = new ImageIcon(getClass().getResource("../tool/lib/iconNoSound.png"));
-    soundLabel = new JButton(soundOn[0] ? soundOnIcon:soundOffIcon);  // put icon depending on playback status (on/off)
+    soundButton = new JButton(soundOn[0] ? soundOnIcon : soundOffIcon);  // put icon depending on playback status (on/off)
+    soundButton.setFocusable(false); // fix: ghost not moving by WASD
 
-    soundLabel.addMouseListener(new MouseAdapter() {
+    soundButton.addMouseListener(new MouseAdapter() {
       @Override
       public void mouseClicked(MouseEvent e) {
         soundOn[0] = !soundOn[0];
         if (!soundOn[0]) {
           stopMusic();
-          soundLabel.setIcon(soundOffIcon);
+          soundButton.setIcon(soundOffIcon);
         } else {
           playMusic();
-          soundLabel.setIcon(soundOnIcon);
+          soundButton.setIcon(soundOnIcon);
         }
       }
     });
 
-    content.add(Box.createRigidArea(new Dimension(0, 15))); // add some spacing between labels
-    soundLabel.setFocusable(false); // fix: ghost not moving by WASD
-    content.add(soundLabel, BorderLayout.CENTER);
+    content.add(Box.createVerticalGlue()); // Add vertical glue to push soundButton to the bottom
+    // center sound button
+    JPanel soundButtonPanel = new JPanel();
+    soundButtonPanel.setOpaque(false);
+    soundButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+    soundButtonPanel.add(soundButton);
+    content.add(soundButtonPanel);
+
     frame.getContentPane().add(content, BorderLayout.CENTER);
     frame.pack();
     frame.setVisible(true);
@@ -101,7 +107,7 @@ public class MazeMenu {
   /**
    * Creates new menu option (button).
    *
-   * @param text text in button
+   * @param text     text in button
    * @param flagName flag to switch
    * @return filled JButton Element for adding to frame
    */
@@ -145,8 +151,8 @@ public class MazeMenu {
         menuElement.removeMouseListener(listener);
       }
     }
-    for (MouseListener listener : soundLabel.getMouseListeners()) {
-      soundLabel.removeMouseListener(listener);
+    for (MouseListener listener : soundButton.getMouseListeners()) {
+      soundButton.removeMouseListener(listener);
     }
   }
 
@@ -164,15 +170,20 @@ public class MazeMenu {
    *
    * @return all flags bool values
    */
-  public boolean menuElementPressed() {return this.flags.get("gameFlag") || this.flags.get("replayFlag") || this.flags.get("exitFlag");}
+  public boolean menuElementPressed() {
+    return this.flags.get("gameFlag") || this.flags.get("replayFlag") || this.flags.get("exitFlag");
+  }
 
   /**
    * Loop through all flags and return the one with value <code>True</code>.
+   *
    * @return menu option (flag) that was pressed
    */
   public String flagEnabled() {
     for (String flag : flags.keySet()) {
-      if (flags.get(flag)) {return flag;}
+      if (flags.get(flag)) {
+        return flag;
+      }
     }
     return null;
   }
