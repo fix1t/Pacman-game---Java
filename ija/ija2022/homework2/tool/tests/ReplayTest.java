d667142e2e8c6d037edf58a3605363ba688afd59
Fix1t <gbiel1117@gmail.com>
2023-05-07 10:10:51 +0200

    add: load maze from file method

---
+++ ija/ija2022/homework2/tool/tests/ReplayTest.java
@@ -0,0 +1,16 @@
+package ija.ija2022.homework2.tool.tests;
+
+import ija.ija2022.homework2.game.Game;
+import ija.ija2022.homework2.tool.common.CommonMaze;
+import org.junit.Before;
+
+import java.nio.file.Path;
+
+public class ReplayTest {
+
+  private CommonMaze maze;
+  private Game game;
+
+
+
+}
